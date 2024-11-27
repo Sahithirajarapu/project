5dbe84effb7cfa11d952566366328f3c913e613a
Sahithirajarapu <sahithi.rajarapu@gmail.com>
2024-11-27 07:24:41 +0530

    Add files via upload

---
+++ javascript practice/basics/script.js
@@ -0,0 +1,16 @@
+alert("hello world");
+console.log("code is running like a wow !!!");
+var a = prompt("Enter a Number");
+console.log("you Number is",+a );
+
+var s = confirm("do you want to succeed");
+
+if(s){
+    console.log("you will succeed");
+
+}
+else{
+    console.log("you will not succeed");
+}
+document.title = "Sahithi";
+document.body.style.backgroundColor = "red";
