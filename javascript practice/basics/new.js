cc485cf108e25e900e71a65d68a8960777810574
Sahithirajarapu <sahithi.rajarapu@gmail.com>
2024-12-16 12:48:16 +0530

    Delete javascript practice directory

--- javascript practice/basics/new.js
+++
@@ -1,4 +0,0 @@
-console.log("code is running like a wow !!!");
-console.log("code is running like a wow !!!");
-console.log("code is running like a wow !!!");
-console.log("code is running like a wow !!!");
